4f5f417ff4a279986649cd111dccbca82042fdfa
Zigguraptor <cobaltum8@gmail.com>
2023-06-27 18:31:12 +0400

    Add Run services

---
+++ KafkaProducerConsumer/Program.cs
@@ -0,0 +1,13 @@
+﻿using KafkaProducerConsumer;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+static IHostBuilder CreateHostBuilder(string[] args) =>
+    Host.CreateDefaultBuilder(args)
+        .ConfigureServices((_, collection) =>
+        {
+            collection.AddHostedService<KafkaProducerHostedService>();
+            collection.AddHostedService<KafkaConsumerHostedService>();
+        });
+
+CreateHostBuilder(args).Build().Run();
